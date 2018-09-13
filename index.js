43f11ac6d8c1f4da9cc72a5652bba100b6852c7b
James Xie <jamesxie2019@u.northwestern.edu>
2018-09-12 20:16:07 -0700

    updated to streetfolio

---
+++ index.js
@@ -0,0 +1,12 @@
+function togglePreviewOn(x) {
+
+   var id = x.id;
+   document.getElementById("p-default").style.display="none";
+   document.getElementById("p-"+id).style.display="block";
+}
+
+function togglePreviewOff(x) {
+  var id = x.id;
+  document.getElementById("p-"+id).style.display="none";
+  document.getElementById("p-default").style.display="block";
+}
